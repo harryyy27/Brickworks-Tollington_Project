24a3dc3e72a0a7e7e5a1b20e28f3abfe203dbce7
EmilyScripts <emily.rever@gmail.com>
2019-01-16 12:27:37 +0000

    style faq page

--- client/src/components/pages/faq.js
+++ client/src/components/pages/faq.js
@@ -9,18 +9,18 @@ class Faq extends React.Component {
         <h1 className='pageHeadings'>FAQs</h1>
         <main>
             <ul>
-                    <li><p className='li-p-strong'>&#8226; &ensp; How does Brickworks connect me with others who can help me develop my idea for a social action?</p>
-                <span className='li-content'>We have 20 trained Community Organisers who can help you develop your idea, build connections with local people, and find others who are interested in the same social action idea as you.</span></li>
-                     <li><p className='li-p-strong'>&#8226; &ensp; I am really concerned about changes to benefits. What can I do?</p>
-                <span className='li-content'>There are a lot of local people who are facing changes due to welfare reforms. We want to help these people find out more about what challenges people are facing and what can be done about it. Get in touch with us and we can put you in contact with people in your community who have the same concerns. You can also use look for  social-action campaigns and events in the area.</span></li>
-                     <li><p className='li-p-strong'>&#8226; &ensp; How can I get younger people to engage more in the community?</p>
-                <span className='li-content'>We're already supporting local people to build social actions to help young people increase their connectivity to their local community. Take a look at our website to see what's currently on offer. If you've got a particular social action idea that you'd like to take forward, then get in touch and we can help you develop it further through one of our Community Organisers.</span></li>
-                     <li><p className='li-p-strong'>&#8226; &ensp; If I list my social action event, where does it appear?</p>
-                <span className='li-content'>Your event can be listed through this App, which means all that are using the App will see your event. We will also list it on our website and through our social media, for examples, we can promote it via Twitter @BrickworksN4.</span></li>
-                     <li><p className='li-p-strong'>&#8226; &ensp; Will it cost me to hire a meeting space at Brickworks and am I limited to specific days or times?</p>
-                <span className='li-content'>As we are developing Brickworks as a Social Action Hub we want to encourage local people to use the facilities this community centre offers. Brickworks is open seven days a week and from 08:00 through to 21:00 most days. We intend to offer the meeting space for free. Get in touch with us at Brickworks to find out more.</span></li>       
-                     <li><p className='li-p-strong'>&#8226; &ensp; How are my personal details that I am entering being used?</p>
-                <span className='li-content'>You can read the Brickworks Social Action Hub privacy policy</span><a href='../../../public/assets/privacy-policy-hcca.pdf'> here</a>.</li>
+                    <li><p className='question'>How does Brickworks connect me with others who can help me develop my idea for a social action?</p>
+                <p className='answer'>"We have 20 trained Community Organisers who can help you develop your idea, build connections with local people, and find others who are interested in the same social action idea as you."</p></li>
+                     <li><p className='question'>I am really concerned about changes to benefits. What can I do?</p>
+                <p className='answer'>"There are a lot of local people who are facing changes due to welfare reforms. We want to help these people find out more about what challenges people are facing and what can be done about it. Get in touch with us and we can put you in contact with people in your community who have the same concerns. You can also use look for  social-action campaigns and events in the area."</p></li>
+                     <li><p className='question'>How can I get younger people to engage more in the community?</p>
+                <p className='answer'>"We're already supporting local people to build social actions to help young people increase their connectivity to their local community. Take a look at our website to see what's currently on offer. If you've got a particular social action idea that you'd like to take forward, then get in touch and we can help you develop it further through one of our Community Organisers."</p></li>
+                     <li><p className='question'>If I list my social action event, where does it appear?</p>
+                <p className='answer'>"Your event can be listed through this App, which means all that are using the App will see your event. We will also list it on our website and through our social media, for examples, we can promote it via Twitter @BrickworksN4."</p></li>
+                     <li><p className='question'>Will it cost me to hire a meeting space at Brickworks and am I limited to specific days or times?</p>
+                <p className='answer'>"As we are developing Brickworks as a Social Action Hub we want to encourage local people to use the facilities this community centre offers. Brickworks is open seven days a week and from 08:00 through to 21:00 most days. We intend to offer the meeting space for free. Get in touch with us at Brickworks to find out more."</p></li>       
+                     <li><p className='question'>How are my personal details that I am entering being used?</p>
+                <p className='answer'>"You can read the Brickworks Social Action Hub privacy policy<a className='faq-anchor' href='../../../public/assets/privacy-policy-hcca.pdf'> here</a>."</p></li>
             </ul>
         </main>
         </div>
--- client/src/components/pages/topten.js
+++ client/src/components/pages/topten.js
@@ -6,41 +6,19 @@ const TopTen= () => (
     <div data-testid="topten-page" className='wrapper'>
         <h1>Top Ten Tips</h1>
         <main>
-            
-        <p className='strong-txt'>Engaging in social action takes time, energy and passion.
-            <br/>
-            Here are our top 10 tips to get you started.</p>
-           <ol>
-               <li>
-               Identify a problem, challenge or issue in your neighbourhood and be specific about what you want to tackle.
-               </li>
-               <li>
-                Do some research so that you have a good picture of the current situation, then develop and plan how to go about tackling the problem.
-                </li>
-                <li>
-                Get together with others to share your research and explore the problem and potential solutions.
-                </li>
-                <li>
-                Build a wider, more diverse community of collaborators who support your aims.
-                </li>
-                <li>
-                Collaborate with those who might not share your opinions or support your aims so that you can overcome objections and gain different perspectives.           
-                </li>
-                <li>
-                Promote your social action in your community and through the media; create a video and post it on the Brickworks website.
-                </li>
-                <li>
-                Take action!
-                </li>
-                <li>
-                Evaluate, reflect and record the impact of your action on the community.
-                </li>
-                <li>
-                Share the impact of your action in creative and imaginative ways to capture attention.
-                </li>
-                <li>Don't give up, have patience and be committed.</li>
-           </ol>
-
+            <p className='question'>Engaging in social action takes time, energy and passion, here are our top 10 tips to get you started:</p>
+            <ol>
+                <li className='top-ten-li'>Identify a problem, challenge or issue in your neighbourhood and be specific about what you want to tackle.</li className='top-ten-li'>
+                <li className='top-ten-li'>Do some research so that you have a good picture of the current situation, then develop and plan how to go about tackling the problem.</li className='top-ten-li'>
+                <li className='top-ten-li'>Get together with others to share your research and explore the problem and potential solutions.</li className='top-ten-li'>
+                <li className='top-ten-li'>Build a wider, more diverse community of collaborators who support your aims.</li className='top-ten-li'>
+                <li className='top-ten-li'>Collaborate with those who might not share your opinions or support your aims so that you can overcome objections and gain different perspectives.</li className='top-ten-li'>
+                <li className='top-ten-li'>Promote your social action in your community and through the media; create a video and post it on the Brickworks website.</li className='top-ten-li'>
+                <li className='top-ten-li'>Take action!</li className='top-ten-li'>
+                <li className='top-ten-li'>Evaluate, reflect and record the impact of your action on the community.</li className='top-ten-li'>
+                <li className='top-ten-li'>Share the impact of your action in creative and imaginative ways to capture attention.</li className='top-ten-li'>
+                <li className='top-ten-li'>Don't give up, have patience and be committed.</li className='top-ten-li'>
+            </ol>
         <div>
             <label htmlFor="all-events">
             <Link to='/contactUs'><p className='link'>Click here to register an interest to start a social action!</p></Link>
@@ -49,7 +27,7 @@ const TopTen= () => (
             <label htmlFor="list-event">
             <Link to='/list-event'><p className='link'>Click here to register an interest to promote a social action</p></Link>
             </label>
-         </div>
+        </div>
         </main>
     </div>
 )
