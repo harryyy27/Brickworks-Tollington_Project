c5df85f00da791ea783229965c9c4ddc9c9c991e
harryyy27 <harryyy27@gmail.com>
2019-01-18 16:39:18 +0000

    fixed other form #7

--- client/src/components/pages/list-event.js
+++ client/src/components/pages/list-event.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {FormErrors} from './eventComp';
-
+import tickIcon from '../../../public/images/tick.png';
+import crossIcon from '../../../public/images/cancel.svg';
 
 class ListEvent extends React.Component {
     state = { 
@@ -39,17 +40,44 @@ console.log('BEFORE SWITCH', this.state.nameValid);
       case 'name':
       nameValid = (value.length >=2 ) 
       fieldValidationErrors.name = nameValid ? '' : ' is too short' ;
+      const nicon = document.getElementById("name-icon");
+      if(nameValid){
+        nicon.src=tickIcon;
+        nicon.className = "validate"
+      }
+      else {
+        nicon.src=crossIcon;
+        nicon.className = "validate"
+      }
       break;
       case 'email':
       //negated emailValid twice as (value.match(..)) gives null or some other value, we need to turn that to boolean, true or false for readability
       emailValid = !(!(value.match(/^([\w.%+-]+)@([\w-]+\.)+([\w]{2,})$/i) ) );
       fieldValidationErrors.email = emailValid ? '' : ' is invalid';
+      const eicon = document.getElementById("email-icon");
+      if(emailValid){
+        eicon.src=tickIcon;
+        eicon.className = "validate"
+      }
+      else {
+        eicon.src=crossIcon;
+        eicon.className = "validate"
+      }
       break;
       
       case 'description':
       // descriptionValid = (value != '');
       descriptionValid = (value.length >= 5);
       fieldValidationErrors.description = descriptionValid ? '' : ' is too short' ;
+      const dicon = document.getElementById("des-icon");
+      if(descriptionValid){
+        dicon.src=tickIcon;
+        dicon.className = "validate"
+      }
+      else {
+        dicon.src=crossIcon;
+        dicon.className = "validate"
+      }
       break;
 
       default:
@@ -75,6 +103,8 @@ console.log('BEFORE SWITCH', this.state.nameValid);
 
   handleSubmit = event => {
     event.preventDefault();
+    if(this.state.formValid){
+    
     const data = JSON.stringify({
       eventProposal: this.state
     })
@@ -94,67 +124,109 @@ console.log('BEFORE SWITCH', this.state.nameValid);
 
       this.setState({ name: '', email: '', phone: '', description: '', eventTitle: '', date: '', time: '', venue: '', hireSpace: false, listEvent: false, formErrors: {name: '', email: '', description: ''}, nameValid: false, emailValid: false, descriptionValid: false, formValid: false });
       alert('your form has been submitted, we will get back to you soon');
-
+    }
+    else {
+      const name = document.getElementById('name');
+      const email = document.getElementById('email');
+      const description = document.getElementById('description');
+      this.validateField('name',name.value);
+      this.validateField('email',email.value);
+      this.validateField('description',description.value);
+    }
   }
 
   render() {
     return ( 
-      <div className='wrapper'>
-      <h1>Contact us</h1>
-      <main>
-      <p className='mobile-p'>Please note that Name, Email and Description are required fields</p>
-      <form>
+      <main className='wrapper'>
+        <h1>Promote a Social Action</h1>
+        <p className='mobile-p'>Please note that Name, Email and Description are required fields</p>
+        <form>
+          <label className='form-label'
+                 htmlFor="name">Name  <span className='red-asterisk'>*</span>
+          </label>
+          <input className='form-box' 
+                type="text" 
+                id="name" 
+                name="name" 
+                value={this.state.name} 
+                onChange= {this.handleChange}/>
+          <img id="name-icon" className="invisible" alt="validation"></img>
+          <label className='form-label' 
+                 htmlFor="email">Email  <span className='red-asterisk'>*</span>
+          </label>
+          <input className='form-box' 
+                 type="text" 
+                 id="email" 
+                 name="email" 
+                 value={this.state.email} 
+                 onChange= {this.handleChange}/>
+          <img id="email-icon" className="invisible" alt="validation"></img>
+          <label className='form-label' 
+                 htmlFor="phone">Telephone Number</label>
+          <input className='form-box' 
+                 type="text" 
+                 id="phone" 
+                 name="phone" 
+                 value={this.state.phone} 
+                 onChange= {this.handleChange}/>
+          <label className='form-label' 
+                 htmlFor="eventTitle">Title of Social Action</label>
+          <input className='form-box' 
+                 type="text" 
+                 id="eventTitle" 
+                 name="eventTitle" 
+                 value={this.state.eventTitle} 
+                 onChange= {this.handleChange}/>
+          <label className='form-label' 
+                 htmlFor="description">Description of Social Action   <span className='red-asterisk'>*</span></label>
+          <textarea name="description" 
+                    id="description" 
+                    cols="40" 
+                    rows="10" 
+                    value={this.state.description} 
+                    onChange={this.handleChange}>
+          </textarea>
+          <img id="des-icon" className="invisible" alt="validation"></img>
 
-        <label className='form-label' htmlFor="name">Name  <span className='red-asterisk'>*</span></label>
-        <input className='form-box' type="text" id="name" name="name" value={this.state.name} onChange= {this.handleChange}/>
-        <label className='form-label' htmlFor="email">Email  <span className='red-asterisk'>*</span></label>
-        <input className='form-box' type="text" id="email" name="email" value={this.state.email} onChange= {this.handleChange}/>
-        <label className='form-label' htmlFor="phone">Telephone Number</label>
-        <input className='form-box' type="text" id="phone" name="phone" value={this.state.phone} onChange= {this.handleChange}/>
-        <label className='form-label' htmlFor="eventTitle">Title of Social Action</label>
-        <input className='form-box' type="text" id="eventTitle" name="eventTitle" value={this.state.eventTitle} onChange= {this.handleChange}/>
-        <label className='form-label' htmlFor="description">Description of Social Action   <span className='red-asterisk'>*</span></label>
-        <textarea name="description" id="description" cols="40" rows="10" value={this.state.description} onChange={this.handleChange}></textarea>
+          <label className='form-label' htmlFor="date">Date</label>
+          <input className='form-box'type="text" id="date" name="date" value={this.state.date} onChange= {this.handleChange}/>
+          <label className='form-label' htmlFor="time">Time</label>
+          <input className='form-box'type="text" id="time" name="time" value={this.state.time} onChange= {this.handleChange}/>
+          <label className='form-label' htmlFor="venue">Venue</label>
+          <input className='form-box'type="text" id="venue" name="venue" value={this.state.venue} onChange= {this.handleChange}/>
 
-        <label className='form-label' htmlFor="date">Date</label>
-        <input className='form-box'type="text" id="date" name="date" value={this.state.date} onChange= {this.handleChange}/>
-        <label className='form-label' htmlFor="time">Time</label>
-        <input className='form-box'type="text" id="time" name="time" value={this.state.time} onChange= {this.handleChange}/>
-        <label className='form-label' htmlFor="venue">Venue</label>
-        <input className='form-box'type="text" id="venue" name="venue" value={this.state.venue} onChange= {this.handleChange}/>
-
-        <p className='choices-p'>Please check the options applicable to you</p>
-        <span className="choices-text">I would like to book space
-        <label className='label-checkbox' htmlFor="hireSpace">
-        <input className='checkbox'
-          type="checkbox"
-          id="hireSpace"
-          name="hireSpace"
-          checked={this.state.hireSpace}
-          onChange={this.handleChange}
-        />
-        </label>
-        </span>
-
-        <span className="choices-text">I would like to list this event
-        <label className="label-checkbox" htmlFor="listEvent">
+          <p className='choices-p'>Please check the options applicable to you</p>
+          <span className="choices-text">I would like to book space
+          <label className='label-checkbox' htmlFor="hireSpace">
           <input className='checkbox'
             type="checkbox"
-            id="listEvent"
-            name="listEvent"
-            checked={this.state.listEvent}
+            id="hireSpace"
+            name="hireSpace"
+            checked={this.state.hireSpace}
             onChange={this.handleChange}
           />
-        </label>
-        </span>
+          </label>
+          </span>
 
-        <button className="button-large" type="submit" onClick={this.handleSubmit} disabled= { !this.state.formValid} >Submit</button>
-      </form>
-      </main>
+          <span className="choices-text">I would like to list this event
+          <label className="label-checkbox" htmlFor="listEvent">
+            <input className='checkbox'
+              type="checkbox"
+              id="listEvent"
+              name="listEvent"
+              checked={this.state.listEvent}
+              onChange={this.handleChange}
+            />
+          </label>
+          </span>
+
+          <button className="button-large" type="submit" onClick={this.handleSubmit} disabled= { !this.state.formValid} >Submit</button>
+        </form>
+      
       <div>
       <FormErrors formErrors={this.state.formErrors} />
     </div>
-    </div>
+    </main>
     )
   }
 }
