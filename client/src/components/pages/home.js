ce889e749ecd3f6732743eeea4c04f36c3c17c0e
EmilyScripts <emily.rever@gmail.com>
2019-01-19 17:13:47 +0000

    add loading animation for home page

--- client/src/components/pages/home.js
+++ client/src/components/pages/home.js
@@ -2,7 +2,7 @@ import React from 'react';
 import logoImg from '../../../public/images/brickworks-logo.svg';
 
 class Home extends React.Component {
-  
+
 componentDidMount = () => {
   window.scrollTo(0,0);
 }
@@ -20,20 +20,17 @@ componentDidMount = () => {
 
   render() {
     return( 
+        <div className='fade-in'>
         <div className='wrapper'>
           <main data-testid="home-page">
-          
-          
             <figure id='logo'>
               <img id='logo-img' src={logoImg} alt='Brickworks logo'/>
               <figcaption className='intro'>Brickworks is a social-action hub rooted in Tollington that actively fosters grass-roots action and supports local people to build the community and create a better society
-            </figcaption>
+              </figcaption>
             </figure> 
-                
-            
-           
             <button className='button-large' onClick={this.addEvent}>Get Involved</button>
           </main>
+        </div>
         </div>
     )}
 }
